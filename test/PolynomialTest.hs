4b59256a1b868952ecdfe60232bc28150c0e1377
Max Mynter <maxmynter@me.com>
2024-09-17 20:00:26 +0200

    (tests) Add PolynomialSplit function

--- test/PolynomialTest.hs
+++ test/PolynomialTest.hs
@@ -1,8 +1,42 @@
 module Main where
 
 import Fft
+import System.Exit (exitFailure)
 import Test.QuickCheck
+
+instance Arbitrary Complex where
+  arbitrary = Complex <$> arbitrary <*> arbitrary
+
+instance Arbitrary Polynomial where
+  arbitrary = Polynomial <$> arbitrary
+
+prop_splitRecombine :: Polynomial -> Property
+prop_splitRecombine p@(Polynomial cs) =
+  let (Polynomial evenPoly, Polynomial oddPoly) = splitPolynomial p
+      recombined = interleave evenPoly oddPoly
+   in counterexample
+        ( "Original: "
+            ++ show cs
+            ++ "\nEven part: "
+            ++ show evenPoly
+            ++ "\nOdd part: "
+            ++ show oddPoly
+            ++ "\nRecombined: "
+            ++ show recombined
+        )
+        (cs == recombined)
+
+interleave :: [a] -> [a] -> [a]
+interleave (x : xs) (y : ys) = x : y : interleave xs ys
+interleave [] ys = ys
+interleave xs [] = xs
 
 main :: IO ()
 main = do
-  putStrLn "Hello, world!"
+  putStrLn "Testing Polynomial"
+  result <- quickCheckResult prop_splitRecombine
+  case result of
+    Success {} -> putStrLn "All tests passed!"
+    _ -> do
+      putStrLn "Test failed!"
+      exitFailure
