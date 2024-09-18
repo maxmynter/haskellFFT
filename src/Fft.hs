f70e4deac44f49bdb4fcae5c74f18ede6ccbdaea
Max Mynter <maxmynter@me.com>
2024-09-18 16:15:55 +0200

    Implement Polar Coordinates

--- src/Fft.hs
+++ src/Fft.hs
@@ -10,21 +10,47 @@ type Re = Double
 
 type Im = Double
 
-data Complex = Complex Re Im deriving (Show, Eq)
+type Mag = Double
+
+type Phi = Double
+
+data Complex = Cartesian Re Im | Polar Mag Phi deriving (Show, Eq)
 
 im :: Complex -> Double
-im (Complex _ i) = i
+im (Cartesian _ i) = i
 
 real :: Complex -> Double
-real (Complex r _) = r
+real (Cartesian r _) = r
+
+mag :: Complex -> Double
+mag (Cartesian r i) = sqrt $ r * r + i * i
+mag (Polar m p) = m
+
+phase :: Complex -> Double
+phase (Polar _ phi) = phi
+phase (Cartesian r i) = atan2 r i
+
+asCartesian :: Complex -> Complex
+asCartesian c = Cartesian (real c) (im c)
+
+asPolar :: Complex -> Complex
+asPolar c = Polar (mag c) (phase c)
 
 instance Num Complex where
-  (+) (Complex a b) (Complex r i) = Complex (a + r) (b + i)
-  (*) (Complex a b) (Complex r i) = Complex (a * r - b * i) (a * i + b * r)
-  negate (Complex a b) = Complex (-a) (-b)
-  signum c@(Complex a b) = let mag = real c in Complex (a / mag) (b / mag)
-  fromInteger n = Complex (fromInteger n) 0
-  abs (Complex a b) = Complex (sqrt (a * a + b * b)) 0
+  (+) (Cartesian a b) (Cartesian r i) = Cartesian (a + r) (b + i)
+  (+) c1 c2 = asCartesian c1 + asCartesian c2
+  (*) (Polar r p) (Polar m t) = Polar (r * m) (p + t)
+  (*) c1 c2 = asPolar c1 * asPolar c2
+  negate (Cartesian a b) = Cartesian (-a) (-b)
+  negate (Polar r p) = Polar (-r) p
+  signum c@(Cartesian a b) = let m = mag c in Cartesian (a / m) (b / m)
+  signum (Polar r p) = signum r
+  fromInteger n = Cartesian (fromInteger n) 0
+  abs c@(Cartesian a b) = Cartesian (mag c) 0
+
+instance Show Complex where
+  show (Cartesian r i) = show r ++ " + " show y ++ "i"
+  show (Polar r p) = show r ++ "∠" ++ show p
 
 splitPolynomial :: Polynomial -> (EvenPolynomial, OddPolynomial)
 splitPolynomial (Polynomial cs) = (Polynomial (evenPart cs), Polynomial (oddPart cs))
@@ -37,5 +63,9 @@ splitPolynomial (Polynomial cs) = (Polynomial (evenPart cs), Polynomial (oddPart
     oddPart [_] = []
     oddPart (_ : y : ys) = y : oddPart ys
 
+unityRoots :: Int -> Int -> Complex
+unityRoots n = [exp ((2 * pi * k * Complex 0 1) / n) | k <- [0 .. (n - 1)]]
+
 fft :: Polynomial -> Polynomial
+fft p@(Polynomial []) = p
 fft cs = undefined
